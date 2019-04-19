538a29595bb767c909ecefd5fd29ab06aa0426da
Nayed Saïd Ali <nayed.saidali@gmail.com>
2019-04-19 22:06:59 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hive-on-hive/precache-manifest.44ad74247e16f0ddc3297634ba9b8607.js"
+  "/precache-manifest.36c36b3d723588a5a1d054021d647bef.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hive-on-hive/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
