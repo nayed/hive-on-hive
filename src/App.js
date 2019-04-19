8a3e6d32cfd861bcd17e5fe09b72df9eb3f50242
Nayed Saïd Ali <nayed.saidali@gmail.com>
2019-04-19 21:07:28 +0200

    ¯\_(ツ)_/¯

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import transform from './transform'
 import './App.css'
 
 class App extends Component {
+  state = { styleOne: {}, styleTwo: {} }
+
+  onMouseMove = event => {
+    this.setState({
+      styleOne: transform(-event.clientX / event.clientY),
+      styleTwo: transform(event.clientX / event.clientY)
+    })
+  }
+
   render() {
-    return <div className="App" />
+    return (
+      <div onMouseMove={this.onMouseMove}>
+        <div className="panel" style={this.state.styleOne} />
+        <div className="panel" style={this.state.styleTwo} />
+      </div>
+    )
   }
 }
 
